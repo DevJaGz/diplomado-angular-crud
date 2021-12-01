28648ac3953d8b754bfbda3e3f4adccdd12c08cd
Julian Gomez <jgomez109@unab.edu.co>
2021-11-30 20:14:51 -0500

    Se finaliza la escritura a la base de datos

--- src/app/components/views/employees/new/new.component.ts
+++ src/app/components/views/employees/new/new.component.ts
@@ -19,7 +19,7 @@ export class NewComponent implements OnInit {
   @BlockUI("block-item") blockUI: NgBlockUI = {} as NgBlockUI;
 
 
-  bussy = false;
+  // bussy = false;
   employeeForm: FormGroup;
 
   constructor(
@@ -39,51 +39,39 @@ export class NewComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // validateDate(control: AbstractControl) {
-  //   if (control.value) {
-  //     const date = moment(control.value).format('DD-MM-YYYY')
-  //     const actualDate = moment().format('DD-MM-YYYY')
-  //     if (date > actualDate) return { limitError: "Fecha mayor a la actual" }
-  //   }
-  //   return null
-  // }
 
   showWarning(controlName: string) {
     return this.employeeForm.get(controlName)!.invalid && this.employeeForm.get(controlName)!.touched
   }
 
 
-  async addEmployee() {
+  addEmployee() {
     if (this.employeeForm.invalid) {
       this.employeeForm.markAllAsTouched()
       return
     }
-    this._blockUIService.start('test')
-    this.setBusy()
     const { value } = this.employeeForm;
-    let error = ""
-    const res = await this._employeeService.addEmployee(value).catch(e => { error = e })
-    // const res: any = await this.testReject().catch(e => { error = e })
-    this.setFree()
-    this._blockUIService.stop('test')
-    if (res) {
-      this.successMessage("Empleado Agregado", "Empleado agregado satisfactoriamente.")
-      this.employeeForm.reset()
-    } else {
-      this.errorMessage("Error", "No fue posible agregar al empleado.", error)
-    }
+
+    this._blockUIService.start('block-target')
+    this._employeeService.addEmployee(value)
+      .then((res) => {
+        console.log("Empleado agregado", res);
+        this._blockUIService.stop('block-target')
+        const { id, parent } = res
+        this.successMessage("Empleado agregado", `El nuevo empleado se ha agregado satisfactoriamente a <b>${parent.path}</b>. Id generado: <b>${id}</b>`)
+        this.employeeForm.reset()
+      })
+      .catch((error) => {
+        this._blockUIService.stop('block-target')
+        this.errorMessage("Error", "No fue posible agregar al empleado.", error)
+      })
   }
 
-  testReject() {
-    return new Promise<any>((resolve, reject) => {
-      reject("error sdfskdjhf a´sk ´fker´f ke")
-    })
-  }
 
   successMessage(title: string, message: string) {
     Swal.fire({
       title: title,
-      text: message,
+      html: message,
       icon: "success",
       confirmButtonText: "OK",
       confirmButtonColor: '#375A7F',
@@ -93,7 +81,7 @@ export class NewComponent implements OnInit {
   errorMessage(title: string, message: string, error: string) {
     Swal.fire({
       title: title,
-      text: message + " " + error,
+      html: message + "<br>" + `<b>${error}</b>`,
       icon: 'error',
       confirmButtonText: "OK",
       confirmButtonColor: '#375A7F',
@@ -101,15 +89,26 @@ export class NewComponent implements OnInit {
   }
 
 
-  setBusy() {
-    this.bussy = true;
-    Object.values(this.employeeForm.controls).forEach(field => field.disable())
-  }
+  // COMPARAR FECHAS
+  // validateDate(control: AbstractControl) {
+  //   if (control.value) {
+  //     const date = moment(control.value).format('DD-MM-YYYY')
+  //     const actualDate = moment().format('DD-MM-YYYY')
+  //     if (date > actualDate) return { limitError: "Fecha mayor a la actual" }
+  //   }
+  //   return null
+  // }
 
-  setFree() {
-    this.bussy = false;
-    Object.values(this.employeeForm.controls).forEach(field => field.enable())
-  }
+  // DESHABILITAR CAMPOS DEL FORMULARIO
+  // setBusy() {
+  //   this.bussy = true;
+  //   Object.values(this.employeeForm.controls).forEach(field => field.disable())
+  // }
+
+  // setFree() {
+  //   this.bussy = false;
+  //   Object.values(this.employeeForm.controls).forEach(field => field.enable())
+  // }
 
 
 }
--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, CollectionReference, DocumentReference } from '@angular/fire/compat/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { IEmployee } from '../interfaces/employee.interface';
 
 @Injectable({
